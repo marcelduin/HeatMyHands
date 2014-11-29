759107a4d2877e8a616829f0f10e18f79fcbe51f
marcelduin <marcel@q42.nl>
2014-11-29 03:18:35 +0100

    t2s && fix slider desktop drag

--- js/iface.js
+++ js/iface.js
@@ -1,7 +1,7 @@
 function Slider(controller, element) {
   this.margin = 32;
-	this.swiping = false;
-	this.currentOn = false;
+  this.swiping = false;
+  this.currentOn = false;
 };
 
 Slider.prototype = {
@@ -16,72 +16,67 @@ Slider.prototype = {
     this.$cnt = $(this._element);
     this.$ = $(this._button);
 
-  	var startX = 0;
-  	var diffX = 0;
-  	var offsetX = 0;
-  	var currentX = 0;
-  	var $win = $(window);
-  	var cntWidth = this.$cnt[0].offsetWidth;
+    var startX = 0;
+    var diffX = 0;
+    var offsetX = 0;
+    var currentX = 0;
+    var $win = $(window);
+    var cntWidth = this.$cnt[0].offsetWidth;
 
-  	function getClientX(e){
-  		if(e.originalEvent) e = e.originalEvent;
-  		if(!/^touch/.test(e.type)) return e.clientX;
-  		else return e.changedTouches[0].clientX;
-  	};
+    function getClientX(e){
+      if(e.originalEvent) e = e.originalEvent;
+      if(!/^touch/.test(e.type)) return e.clientX;
+      else return e.changedTouches[0].clientX;
+    };
   
-  	function sStart(e){
-  		if(self.swiping || (e.type == 'mousedown' && e.which != 1)) return;
-  		e.stopPropagation();
-  		e.preventDefault();
-  		$win.on('touchmove mousemove',sMove)
-  		  .on('touchend touchcancel touchleave mouseup click',sStop);
+    function sStart(e){
+      if(self.swiping || (e.type == 'mousedown' && e.which != 1)) return;
+      e.stopPropagation();
+      e.preventDefault();
+      $win.on('touchmove mousemove',sMove)
+        .on('touchend touchcancel touchleave mouseup click',sStop);
 
-  		self.$cnt[0].classList.add('swiping');
-  		startX = getClientX(e);
-  	};
-  	function sMove(e){
-  		e.stopPropagation();
-  		e.preventDefault();
-  		diffX = getClientX(e) - startX;
-  		if(self.swiping || Math.abs(diffX) > 5) {
-  			self.swiping = true;
-  			self.moveTo(offsetX+diffX);
-  		}
-  	};
-  	function sStop(e){
-  		setTimeout(function(){
-  			self.swiping = false;
-  		},10);
-  		e.stopPropagation();
-  		e.preventDefault();
-  		$win.off('touchmove mousemove',sMove)
-  		  .off('touchend touchcancel touchleave mouseup click',sStop);
-
-  		self.$cnt[0].classList.remove('swiping');
+      self.swiping = true;
+      startX = getClientX(e);
+    };
+    function sMove(e){
+      e.stopPropagation();
+      e.preventDefault();
+      diffX = (innerWidth>520 && 2 || 1) * (getClientX(e) - startX);
+      self.moveTo(offsetX+diffX);
+    };
+    function sStop(e){
+      setTimeout(function(){
+        self.swiping = false;
+      },10);
+      e.stopPropagation();
+      e.preventDefault();
+      $win.off('touchmove mousemove',sMove)
+        .off('touchend touchcancel touchleave mouseup click',sStop);
 
       self.moveTo(offsetX+diffX);
-  		offsetX = currentX;
+      offsetX = currentX;
   
-  		// Snap to closest to center
-  		snap();
-  	};
+      // Snap to closest to center
+      snap();
+    };
   
-  	function snap(){
-  	  var onOff = Math.round(offsetX/(cntWidth-2*self.margin));
-  	  if(onOff != self.currentOn && self.onchange) self.onchange(!!onOff);
-  	  self.currentOn = onOff;
-  	  self.moveTo(onOff?cntWidth:0);
-  	  offsetX = currentX;
-  	};
+    function snap(){
+      var onOff = Math.round(offsetX/(cntWidth-2*self.margin));
+      if(onOff != self.currentOn && self.onchange) self.onchange(!!onOff);
+      self.currentOn = onOff;
+      self.moveTo(onOff?cntWidth:0);
+      offsetX = currentX;
+    };
 
-  	this.moveTo = function(x) {
-  	  var w = cntWidth - 2*self.margin;
-			self.$[0].style.left = (currentX = Math.max(0, Math.min(w, x)))+'px';
-			self._off.style.width = (w-currentX)+'px';
-			self._on.style.width = currentX+'px';
-  	};
+    this.moveTo = function(x) {
+      var w = cntWidth - 2*self.margin;
+      self.$[0].style.left = (currentX = Math.max(0, Math.min(w, x)))+'px';
+      self._off.style.width = (w-currentX)+'px';
+      self._on.style.width = currentX+'px';
+    };
 
-		this.$.on('touchstart mousedown',sStart);
+    this.$.on('touchstart mousedown',sStart);
 
   }
 };
